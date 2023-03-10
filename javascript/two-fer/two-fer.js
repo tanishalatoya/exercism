10b69816483224229130ddd71436eea4ca453f27
Tanisha L Davey <tanisha.davey2@gmail.com>
2023-03-09 22:52:50 -0700

    Added solution for two-fer.js

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -3,6 +3,10 @@
 // convenience to get you started writing code faster.
 //
 
-export const twoFer = () => {
-  throw new Error('Remove this statement and implement this function');
+export const twoFer = (name = 'you') => {
+  let message = 'One for you, one for me.';
+  if (name !== 'you') {
+    message = `One for ${name}, one for me.`
+  }
+  return message;
 };
